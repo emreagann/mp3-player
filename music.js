f0d2e9ac7bad8104855b3f6cad30ff489dacc630
emreagann <159646253+emreagann@users.noreply.github.com>
2025-04-24 17:47:46 +0300

    Update music.js

--- music.js
+++ music.js
@@ -16,4 +16,5 @@ const musicList = [
  new Music("Ayayay","Bratia Stereo","2.jpg","2.mp3"),
  new Music("Hips Don't Lie","Shakira","3.jpg","3.mp3"),
  new Music("APT","Rose","4.jpeg","4.mp3"),
+ new Music("Bir Ateşe Attın Beni","Kamuran Akkor","5.jpg","5.mp3"),
 ];
