25e3c2cdb0de3d3d46039391a970282ae83844cb
emreagann <159646253+emreagann@users.noreply.github.com>
2025-05-30 13:56:44 +0300

    Update app.js

--- app.js
+++ app.js
@@ -115,7 +115,7 @@ const displayMusicList = (musicList) => {
   for(let i = 0; i < musicList.length; i++){
     let li = `   <li li-index='${i}' onclick="selectedMusic(this)" class="list-group-item d-flex justify-content-between align-items-center">
                     <span>${musicList[i].getName()}</span>
-                    <span id="music-${i}" class="badge bg-primary rounded-pill"></span>
+                    <span id="music-${i}" class="badge bg-primary rounded-pill">3:17</span>
                     <audio class="music-${i}" src="mp3/${musicList[i].file}"></audio>
                 </li>`;
                 ul.insertAdjacentHTML("beforeend", li);
